1d3e627bbcd100097c3dfc25f3e1ae107d57ac14
Shin ae KWON <shikwon@f0r7s11.clusters.42paris.fr>
2025-06-26 20:40:00 +0200

    Let's start ex404

---
+++ ex04/load_image.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+
+def ft_load(path: str) -> list:
+    """
+    This function allows to load an image file.
+    Input should be (path + ) filename. 
+    """
+
+    try:
+        img = Image.open(path)
+        array = np.array(img)
+        print(f"The shape of image is {array.shape}")
+        print(array)
+        return(array)
+
+    except Exception as e:
+        print("Unhandled error has occured:", e)
+
+
